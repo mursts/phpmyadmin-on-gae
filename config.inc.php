f60d1452df719994402648f7787e08dfd22ddeac
mursts <mursts@gmail.com>
2019-06-06 01:38:48 +0900

    update config.inc.php for Cloud SQL

--- config.inc.php
+++ config.inc.php
@@ -14,7 +14,7 @@
  * This is needed for cookie based authentication to encrypt password in
  * cookie. Needs to be 32 chars long.
  */
-$cfg['blowfish_secret'] = ''; /* YOU MUST FILL IN THIS FOR COOKIE AUTH! */
+$cfg['blowfish_secret'] = '3V)MNWd)3VWzpe2qY8mclcKSV[-|QmD#{a}dC0KL'; /* YOU MUST FILL IN THIS FOR COOKIE AUTH! */
 
 /**
  * Servers configuration
@@ -28,7 +28,7 @@ $i++;
 /* Authentication type */
 $cfg['Servers'][$i]['auth_type'] = 'cookie';
 /* Server parameters */
-$cfg['Servers'][$i]['host'] = 'localhost';
+$cfg['Servers'][$i]['socket'] = '/cloudsql/freegufo-storage:asia-northeast1:fg-storage-ja';
 $cfg['Servers'][$i]['compress'] = false;
 $cfg['Servers'][$i]['AllowNoPassword'] = false;
 
